ca1077940454f93b648c0f21abb5ab91fb18b374
Carlos Beraun <cberaun2@cnm.edu>
2015-10-15 08:52:53 -0600

    Added gitignore to DDP data design project - reddit

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 				Reddit is a combination of a social network site with user applied news feeds and forums that have several categories and topics from the most common like ex. science, technology, Gaming, Food etc.. to the more obscure. reddit also allows members create their own forums and categories/sub reddits. Members have the ability to send and receive private messages, reddit's submitting and comment abilities are extensive . Reddit allows a user to reply/comment not only to the reddit post itself but also to any user who has also previously commented on the same reddit post.
 			</p>
 
-		<h3>*PERSONA#1 “Bill"</h3>
+		<h3>*PERSONA#1 “Billy"</h3>
 		<p>
-			Bill is a young adult ranging from ages 18-35. Bill is a information driven individual and concernedwith world news and events that are not talked about in
-			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Bill is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.
+			Billy is a young adult ranging from ages 18-35. Billy is a information driven individual and concernedwith world news and events that are not talked about in
+			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Billy is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.
 		</p>
 
 		<h3>*PERSONA#2 "Bobby"</h3>
@@ -23,6 +23,22 @@
 			helikes fast cars and to party. His drink of choice is vodka red bull. he and has 10 empty monster energy drink cans on the floor of his tricked out 2005 Honda civic.
 			Bobby likes to wear fox motocross t-shirts with white sunglasses and a backward baseball hat. Bobby is a me guy, bobby likes to talk about himself and does not care about anyone. If Bobby does not like you he will try to fight you. Bobby is likely to put people down for no reason and justify his actions as him just trying to have some fun.
 		</p>
+
+		<h2>Use Case for Billy and Bobby</h2>
+		<h3>Billy</h3>
+		<ol>
+			<li>type url to browser.</li>
+			<li>logging to user account using registered username & email.</li>
+			<li>Users landing page opens.</li>
+			<li>User reviews and replies to messages.</li>
+			<li>reads and replies to comments to their posting.</li>
+			<li>User seeks out literature/articles that interest them.</li>
+			<li>user reads articles</li>
+			<li>submits up or down votes.</li>
+			<li>comments on articles.</li>
+			<li> user repeats steps 6-9 until satisfied.</li>
+			<li>user logs out.</li>
+		</ol>
 	</body>
 
 </html>
