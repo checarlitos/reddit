00bb9b44ea363d8b7ed0132cce49371654d69345
Carlos Beraun <cberaun2@cnm.edu>
2015-10-15 08:12:02 -0600

    Added gitignore to DDP data design project - reddit

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
 	</head>
 
 	<body>
-		<p></p>
-	</body>
+		<header>REDDIT PROJECT</header>
+		<h1>*PERSONA#1 “Bill"</h1>
+		<p>
+			Bill is a young adult ranging from ages 18-35. Bill is a information driven individual and concerned with world news and events that are not talked about in
+			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Bill is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.</p>
+	</body>h
 </html>
