79c8c912a1efeca5c593ddeff74f20dfaa05ad42
Carlos Beraun <cberaun2@cnm.edu>
2015-10-15 08:35:35 -0600

    Added gitignore to DDP data design project - reddit

--- index.php
+++ index.php
@@ -5,10 +5,12 @@
 	</head>
 
 	<body>
-		<header>REDDIT PROJECT</header>
-		<h1>*PERSONA#1 “Bill"</h1>
+		<header>REDDIT DATA DESIGN PROJECT</header>
+		<h3>reddit.com</h3>
+			<p>Reddit is a combination of a social network and a user applied news feed and forum that have several categories ex. science, technology, Gaming, Food etc..  reddit also allows members create their own forums and categories/sub reddits. Members have the ability to send and receive private messages, reddit's submitting and comment abilities are extensive . Reddit allows a user to reply/comment not only to the reddit post itself but also to any user who has also previously commented on the same reddit post.</p>
+		<h2>*PERSONA#1 “Bill"</h2>
 		<p>
 			Bill is a young adult ranging from ages 18-35. Bill is a information driven individual and concerned with world news and events that are not talked about in
 			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Bill is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.</p>
-	</body>h
+	</body>
 </html>
