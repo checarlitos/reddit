c69ef1b03d9d8ddf16a47c92f87be9cd539f7cfb
Carlos Beraun <cberaun2@cnm.edu>
2015-10-15 08:43:42 -0600

    Added gitignore to DDP data design project - reddit

--- index.php
+++ index.php
@@ -7,10 +7,22 @@
 	<body>
 		<header><h1>REDDIT DATA DESIGN PROJECT</h1></header>
 		<h2>reddit.com</h2>
-			<p>Reddit is a combination of a social network site with user applied news feeds and forums that have several categories and topics from the most common like ex. science, technology, Gaming, Food etc.. to the more obscure. reddit also allows members create their own forums and categories/sub reddits. Members have the ability to send and receive private messages, reddit's submitting and comment abilities are extensive . Reddit allows a user to reply/comment not only to the reddit post itself but also to any user who has also previously commented on the same reddit post.</p>
+			<p>
+				Reddit is a combination of a social network site with user applied news feeds and forums that have several categories and topics from the most common like ex. science, technology, Gaming, Food etc.. to the more obscure. reddit also allows members create their own forums and categories/sub reddits. Members have the ability to send and receive private messages, reddit's submitting and comment abilities are extensive . Reddit allows a user to reply/comment not only to the reddit post itself but also to any user who has also previously commented on the same reddit post.
+			</p>
+
 		<h3>*PERSONA#1 “Bill"</h3>
 		<p>
-			Bill is a young adult ranging from ages 18-35. Bill is a information driven individual and concerned <br>with world news and events that are not talked about in
-			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Bill is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.</p>
+			Bill is a young adult ranging from ages 18-35. Bill is a information driven individual and concernedwith world news and events that are not talked about in
+			the main steam media. Bills scene of humor can be slightly cold and dark and is attracted to the oddities that the web has offers. Bill is in touch with the worlds modern technologies, advancements and is tech savvy. He appears to be is a open minded, liberal truth seeker. Bill is cynical towards corporate america.
+		</p>
+
+		<h3>*PERSONA#2 "Bobby"</h3>
+		<p>
+			Bobby is a young adult between 18-35,
+			helikes fast cars and to party. His drink of choice is vodka red bull. he and has 10 empty monster energy drink cans on the floor of his tricked out 2005 Honda civic.
+			Bobby likes to wear fox motocross t-shirts with white sunglasses and a backward baseball hat. Bobby is a me guy, bobby likes to talk about himself and does not care about anyone. If Bobby does not like you he will try to fight you. Bobby is likely to put people down for no reason and justify his actions as him just trying to have some fun.
+		</p>
 	</body>
+
 </html>
