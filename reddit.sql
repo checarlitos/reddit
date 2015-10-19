4c425fc8ff08c92a6b0013db787011075ea2f231
Carlos Beraun <cberaun2@cnm.edu>
2015-10-19 13:45:44 -0600

    Added MYSQL page

---
+++ reddit.sql
@@ -0,0 +1,33 @@
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS ruser;
+
+CREATE TABLE ruser (
+		userPref INT UNSIGNED AUTO_INCREMENT NOT NULL,
+		userHandle VARCHAR(32) NOT NULL,
+		UNIQUE(userHandle),
+		PRIMARY KEY(userPref)
+);
+
+CREATE TABLE post (
+		subReddit INT USIGNED AUTO_INCREMENT NOT NULL,
+		userHandle INT UNSIGNED NOT NULL,
+		content VARCHAR (140) NOT NULL,
+		vote
+		INDEX (userHandle),
+		FOREIGN KEY (userHandle) REFERENCES ruser(userHandle),
+		PRIMARY KEY (subReddit)
+);
+
+CREATE TABLE comments (
+		subPost INT UNSIGNED AUTO_INCREMENT NOT NULL,
+		userHandle INT UNSIGND NOT NULL,
+		content VARCHAR (140) NOT NULL,
+		vote
+		INDEX (userHandle),
+		FOREIGN KEY (userHandle) REFERENCES (userHandle),
+		FOREIGN KEY (subPost) REFERENCES subPost (subPost),
+		PRIMARY KEY (userHandle, subPost)
+);
+
+
