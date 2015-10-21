91bda68e31482a4dee2ab77ddbe620e471d8ed3f
Carlos Beraun <cberaun2@cnm.edu>
2015-10-21 14:27:35 -0600

    Added Doc Block to page

---
+++ non-foreign-key-class.php
@@ -0,0 +1,8 @@
+<?php
+/**
+*Typical post for a Social Networking site.
+ * this post is a example of data collected and stored about a user for social networking pourposes
+ *
+ * @author Carlos Beraun AKA CarlosMacUser cberaun2@cnm.edu
+**/
+?>
