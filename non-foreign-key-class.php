3b705c3c6ce1185f2421a50cb9779627768a813f
Carlos Beraun <cberaun2@cnm.edu>
2015-10-21 15:11:45 -0600

    Stated coding and adding class

--- non-foreign-key-class.php
+++ non-foreign-key-class.php
@@ -5,4 +5,43 @@
  *
  * @author Carlos Beraun AKA CarlosMacUser cberaun2@cnm.edu
 **/
+
+class Post {
+	/**
+	 * id for this Post: this is the primary key
+	 */
+	private $postId;
+
+
+	/**
+	 * ID for this postHandel if for who wrote this post; this is the foreign key
+	 */
+	private $postHandelId;
+
+
+	/**
+	 * post vote for this post
+	 */
+	private $postVote;
+
+	/**
+	 * Post content for this post
+	 */
+	private $postComment;
+
+	/**
+	 * accessor method for post id
+	 *
+	 * @return int value of post id
+	 */
+	public function getpostId() {
+		return ($this-> postId);
+
+	}
+}
+
+
+
 ?>
+
+
