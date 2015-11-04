63e1c5e6ee9064dcd7d5186bb0e4effebbe1c4c1
Carlos Beraun <cberaun2@cnm.edu>
2015-11-04 16:52:33 -0700

    Added "get all posts" block

--- bread_admin_classes.php
+++ bread_admin_classes.php
@@ -9,31 +9,37 @@
 class Administrator {
 	/**
 	 * Id for this Administrator: this is the primary key
+	 * @var int $adminId
 	 */
 	private $adminId;
 
 	/**
- * Id for this Volunteer is for who claims a listing; this is the foreign key
- */
+	*Id for this Volunteer is for the Volunteer that makes a claim on a listing; this is the foreign key
+	* @var int $volId
+	*/
 	private $volId;
 
 	/**
 	 * Id for this Organization is for the organization that claims listing; this is the foreign key
+	 * @var
 	 */
 	private $orgId;
 
 	/**
 	 * Id for the The Administrator Email address.
+	 * @var string $adminEmail
 	 */
 	private $adminEmail;
 
 	/**
 	 * Id for the activation of Administrators Email address.
+	 * @var int $adminEmailActivation
 	 */
 	private $adminEmailActivation;
 
 	/**
 	 * Id for the The Administrator users First Name
+	 * @var string $adminFirstName
 	 */
 	private $adminFirstName;
 
